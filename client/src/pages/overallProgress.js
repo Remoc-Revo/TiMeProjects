50aadda19439cd3d9f136de56d41dd56855b5336
Tim <timothynguono@gmail.com>
2022-11-22 09:45:27 +0300

    added functionality of displaying progress' overall progress

--- client/src/pages/overallProgress.js
+++ client/src/pages/overallProgress.js
@@ -7,76 +7,12 @@ import { useNavigate } from "react-router-dom";
 export default function OverallProgress(){
     const navigate=useNavigate();
 
-    var [tasksMarkup,set_tasksMarkup]=useState([]);
+    var [tasks,setTasks]=useState([]);
 
     useEffect(()=>{
         axios.get('http://localhost:3001/overall_progress')
              .then((response)=>{
-                const tasks=response.data;
-
-                var tasksMarkup_buffer=[];
-                tasks.forEach(task=>{
-                    document.write(task)
-
-                    // tasksMarkup_buffer.push(
-                    //     <tr>
-                    //         <td id={'taskName'+task.taskId}>
-                    //         </td>
-                    //         <td>
-                                                                                    
-                    //             <div class="d-flex align-items-center">
-                    //                 <img src="../images/user-alt.png"
-                    //                     alt=""
-                    //                     style={{width:'40px',height:'40px'}}
-                    //                     class="rounded-circle"
-                    //                 />
-                    //                 <div class="ms-3">
-                    //                     {/* <p class="fw-bold mb-1">{task.requirementHandlerDetails.userName}</p> */}
-                    //                     {/* <p class="text-muted mb-0" id="handler">{task.requirementHandlerDetails.userEmail}</p> */}
-                    //                 </div>
-                                
-                    //                 <div class="ms-3">
-                    //                     <p style={{color:'orangered'}} id="handler">unasigned</p>
-                    //                 </div>
-                                    
-                    //             </div>
-                    //         </td>
-
-                            
-                    //             <td id={'startDate'+task.taskId}>
-                    //                 {/* {task.startDate} */}
-                    //             </td>
-                            
-                    //             <td id={'startDate'+task.taskId} style={{color:'orangered'}}>unasigned</td>
-                            
-                    //             <td id={'endDate'+task.taskId}>
-                    //                 {/* {task.endDate} */}
-                    //             </td>
-                    //             <td id={'endDate'+task.taskId} style={{color:'orangered'}}>unasigned</td>
-                            
-                            
-                    //         <td>
-                                
-                    //                 <button class="btn btn-link btn-sm btn-rounded" data-mdb-toggle="modal" 
-                    //                      data-mdb-target="#editTask">
-                    //                     EDIT
-                    //                 </button>
-                                
-                    //                 <button class="btn btn-link btn-sm btn-rounded" data-mdb-toggle="modal" 
-                    //                          data-mdb-target="#editTask">
-                    //                     EDIT
-                    //                 </button>
-                                
-
-                    //         </td>
-                    //         <td>
-                    //             <p style={{color:'green'}}>DONE</p>
-                    //         </td>
-                    //     </tr>
-                    // );
-                })
-                set_tasksMarkup(tasksMarkup_buffer);
-                document.write("the taskss",tasksMarkup)
+                setTasks(response.data.tasks);
              })
              .catch((err)=>{
                 if(err.response.status===401){
@@ -84,9 +20,10 @@ export default function OverallProgress(){
                 }
              })
 
-
-             
+  
     },[])
+
+  
     return(
         <div >
             <MainNav/>
@@ -110,7 +47,86 @@ export default function OverallProgress(){
                             </tr>
                         </thead>
                         <tbody>
-                                {tasksMarkup}                            
+                                {/* {tasksMarkup}                             */}
+                                {tasks.map(task=>{                                    
+                                    var handler;
+                                    var startDate;
+                                    var endDate;
+
+                                    // handler details
+                                    if(typeof task.requirementHandlerDetails !='undefined'){
+                                        handler=<div class="d-flex align-items-center">
+                                                    <img src={require("../user-alt.png")}
+                                                            alt=""
+                                                            style={{width:'40px',height:'40px'}}
+                                                            class="rounded-circle"
+                                                        />
+                                                        <div class="ms-3">
+                                                            <p class="fw-bold mb-1">{task.requirementHandlerDetails.userName}</p>
+                                                            <p class="text-muted mb-0" id="handler">{task.requirementHandlerDetails.userEmail}</p>
+                                                        </div>
+
+                                                 </div>
+                                    }
+                                    else{
+                                        handler=<div class="ms-3">
+                                                    <p style={{color:'orangered'}} id="handler">unasigned</p>
+                                                </div>
+                                    }
+
+                                    //startDate
+                                    if(task.startDate!=null){
+                                        startDate=<p id={'startDate'+task.taskId}>
+                                                        {task.startDate}
+                                                    </p>
+
+                                    }
+                                    else{
+                                        startDate=<p id={'startDate'+task.taskId}style={{color:'orangered'}}>unasigned</p>
+                                    }
+
+
+                                    //endDate
+                                    if(task.endDate!=null){
+                                        endDate=<td id={'endDate'+task.taskId}>
+                                                        {task.endDate}
+                                                </td>
+                                    }
+                                    else{
+                                        endDate=<td id={'endDate'+task.taskId} style={{color:'orangered'}}>unasigned</td>
+                                    }
+
+                                    return  <tr>
+                                                <td id={'taskName'+task.taskId}>
+                                                    {task.taskName}
+                                                </td>
+                                                <td>
+                                                         {handler}                                             
+                                                </td>
+                                                                         
+                                                <td >
+                                                    {startDate}
+                                                </td>
+                                                   
+                                                <td>
+                                                   {endDate} 
+                                                </td>
+                                                
+                                                
+                                                <td>
+                                                                                                        
+                                                        <button class="btn btn-link btn-sm btn-rounded" data-mdb-toggle="modal" 
+                                                                data-mdb-target="#editTask">
+                                                            EDIT
+                                                        </button>
+                                                    
+
+                                                </td>
+                                                <td>
+                                                    <p style={{color:'green'}}>DONE</p>
+                                                </td>
+                                            </tr>
+                                })}
                         </tbody>
                     </table>
                 </div>
